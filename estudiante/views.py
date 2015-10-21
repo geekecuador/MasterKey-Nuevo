54c7adf1bf9f11061f68d65fd679b6b444152e17
David Pulloquinga <david@pulloquinga.com>
2015-10-21 07:59:22 -0500

    Cambios ultimos

--- estudiante/views.py
+++ estudiante/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponseRedirect
 from itertools import islice, chain
 from estudiante.models import Taller,Curso,Academic_Rank
 from contrato.models import Estudiante
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -42,7 +43,6 @@ def login_user(request):
                         filter(tipo_leccion__in=range(usuario.estudiante.nivel.leccion-5,usuario.estudiante.nivel.leccion+6)).\
                         filter(tipo_nivel=usuario.estudiante.nivel.nivel).filter(sede=usuario.estudiante.sede)
                     cursos2 = Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).\
-                        filter(tipo_leccion__in=range(usuario.estudiante.nivel.leccion-5,usuario.estudiante.nivel.leccion+6)).\
                         filter(tipo_nivel='xx').filter(sede=usuario.estudiante.sede)
                     cursos = list(chain(cursos1,cursos2))
                     print cursos
@@ -97,30 +97,74 @@ class Busqueda_info_ajax(TemplateView):
         print data
         return HttpResponse(data, content_type="application/json")
 
-
+@login_required
 def reserva(request):
+    estado = False
     if request.method == 'POST':
         user = request.user.id
         usuario = User.objects.get(id=user)
+        fotourl = usuario.estudiante.foto.url
+        cedula = usuario.estudiante.cedula
+        telefono = usuario.estudiante.telefono
+        fecha = datetime.datetime.today()
+        programa = usuario.estudiante.programa.nombre_del_programa
+        duracion = usuario.estudiante.fecha_de_expiracion
+        startdate = datetime.date.today()+ datetime.timedelta(days=1)
+        enddate = startdate + datetime.timedelta(days=6)
+        nivel  = usuario.estudiante.nivel
+        cursos = []
+        talleres = []
         taller = request.POST.get('talleres')
         taller_actualizar = Taller.objects.get(pk=taller)
+        if Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).count() > 0 :
+            cursos1 = Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).\
+                        filter(tipo_leccion__in=range(usuario.estudiante.nivel.leccion-5,usuario.estudiante.nivel.leccion+6)).\
+                        filter(tipo_nivel=usuario.estudiante.nivel.nivel).filter(sede=usuario.estudiante.sede)
+            cursos2 = Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).\
+                        filter(tipo_nivel='xx').filter(sede=usuario.estudiante.sede)
+            cursos = list(chain(cursos1,cursos2))
+            print cursos
+            talleres = Taller.objects.filter(nivel=usuario.estudiante.nivel).filter(fecha__range=[startdate, enddate]).filter(capacidad__gt=0).filter(lugar=usuario.estudiante.sede)
+            print talleres
         if taller_actualizar.alumnos.all().filter(pk=usuario.estudiante.cedula).count() == 0:
             taller_actualizar.alumnos.add(usuario.estudiante)
             taller_actualizar.capacidad = taller_actualizar.capacidad - 1
             taller_actualizar.save()
-
-            return HttpResponse('Registro exitoso')
+            estado = True
+            return render(request,'contenido.html',{'username':request.user,'fecha':fecha,'duracion':duracion,'fotourl':fotourl,'cedula':cedula,'telefono':telefono,'programa':programa,'talleres':talleres,'cursos':cursos,'nivel':nivel,'estado1':estado})
         else:
-            return HttpResponse('Usted ya se encuentro registrado')
+            return render(request,'contenido.html',{'username':request.user,'fecha':fecha,'duracion':duracion,'fotourl':fotourl,'cedula':cedula,'telefono':telefono,'programa':programa,'talleres':talleres,'cursos':cursos,'nivel':nivel,'estado1':estado})
     else:
-        return HttpResponse('Error en el formulario, por favor contactarse con la administracion.')
-
+        return render(request,'contenido.html',{'estado':estado})
+@login_required
 def reservar_curso(request):
+    estado = False
     if request.method == 'POST':
         user = request.user.id
         usuario = User.objects.get(id=user)
+        fotourl = usuario.estudiante.foto.url
+        cedula = usuario.estudiante.cedula
+        telefono = usuario.estudiante.telefono
+        fecha = datetime.datetime.today()
+        programa = usuario.estudiante.programa.nombre_del_programa
+        duracion = usuario.estudiante.fecha_de_expiracion
+        startdate = datetime.date.today()+ datetime.timedelta(days=1)
+        enddate = startdate + datetime.timedelta(days=6)
+        nivel  = usuario.estudiante.nivel
+        cursos = []
+        talleres = []
         curso = request.POST.get('cursos')
         curso_actualizar = Curso.objects.get(pk=curso)
+        if Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).count() > 0 :
+            cursos1 = Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).\
+                        filter(tipo_leccion__in=range(usuario.estudiante.nivel.leccion-5,usuario.estudiante.nivel.leccion+6)).\
+                        filter(tipo_nivel=usuario.estudiante.nivel.nivel).filter(sede=usuario.estudiante.sede)
+            cursos2 = Curso.objects.filter(fecha__range=[startdate, enddate]).filter(capacidad_maxima__gt=0).\
+                        filter(tipo_nivel='xx').filter(sede=usuario.estudiante.sede)
+            cursos = list(chain(cursos1,cursos2))
+            print cursos
+            talleres = Taller.objects.filter(nivel=usuario.estudiante.nivel).filter(fecha__range=[startdate, enddate]).filter(capacidad__gt=0).filter(lugar=usuario.estudiante.sede)
+            print talleres
         if curso_actualizar.tipo_nivel == 'xx' or curso_actualizar.tipo_leccion == 0:
             curso_actualizar.tipo_nivel = usuario.estudiante.nivel.nivel
             curso_actualizar.tipo_leccion = usuario.estudiante.nivel.leccion
@@ -132,13 +176,14 @@ def reservar_curso(request):
                 curso_actualizar.capacidad_maxima = curso_actualizar.capacidad_maxima - 1
                 curso_actualizar.tipo_estudiante.add(usuario.estudiante.nivel)
                 curso_actualizar.save()
-                return HttpResponse('Registro Exitoso')
+                estado = True
+                return render(request,'contenido.html',{'username':request.user,'fecha':fecha,'duracion':duracion,'fotourl':fotourl,'cedula':cedula,'telefono':telefono,'programa':programa,'talleres':talleres,'cursos':cursos,'nivel':nivel,'estado':estado})
             else:
-                return HttpResponse('Usted ya se encuentra registrado')
+                return render(request,'contenido.html',{'username':request.user,'fecha':fecha,'duracion':duracion,'fotourl':fotourl,'cedula':cedula,'telefono':telefono,'programa':programa,'talleres':talleres,'cursos':cursos,'nivel':nivel,'estado':estado})
         else:
-            return HttpResponse('El curso seleccionado,se encuentra fuera del rango. Por favor seleccione otro curso.')
+            return render(request,'contenido.html',{'username':request.user,'fecha':fecha,'duracion':duracion,'fotourl':fotourl,'cedula':cedula,'telefono':telefono,'programa':programa,'talleres':talleres,'cursos':cursos,'nivel':nivel,'estado':estado})
     else:
-        return HttpResponse('Error en la reserva, por favor contactarse con la administracion.')
+        return render(request,'contenido.html',{'estado:':estado})
 
 def update(request, pullo):
     if request.method == 'POST':
@@ -150,7 +195,7 @@ def update(request, pullo):
     else:
 
         return render_to_response('account.html')
-
+@login_required
 def academic_rank(request):
     user = request.user.id
     usuario = User.objects.get(id=user)
